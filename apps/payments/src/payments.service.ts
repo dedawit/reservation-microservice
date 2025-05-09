bced5ca265af0295e2b34035f8ce7c1babc0bf96
dedawit <realdavis7779@gmail.com>
2025-05-09 21:12:13 +0300

    stripe payment microservice setup

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Stripe from 'stripe';
+import { CreateChargeDto } from './dto/create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
@@ -12,10 +13,20 @@ export class PaymentsService {
   );
   constructor(private readonly configService: ConfigService) {}
 
-  async createCharge(card: Stripe.PaymentMethodCreateParams.Card){
+  async createCharge({ card, amount }: CreateChargeDto) {
     const paymentMethod = await this.stripe.paymentMethods.create({
       type: 'card',
-      card:
-    })
+      card,
+    });
+
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      payment_method: paymentMethod.id,
+      amount: amount * 100,
+      confirm: true,
+      payment_method_types: ['card'],
+      currency: 'usd',
+    });
+
+    return paymentIntent;
   }
 }
