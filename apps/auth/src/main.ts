653a932b2915b21a1ba05bfb5cbebe2c748bd9fe
dedawit <realdavis7779@gmail.com>
2025-05-09 14:08:29 +0300

    auth module and reservations module completed

---
+++ apps/auth/src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AuthModule } from './auth.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from 'nestjs-pino';
+import * as cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AuthModule);
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  // app.useLogger(app.get(Logger));
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
+}
+bootstrap();
